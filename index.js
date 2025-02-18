0f709ca9624ac6b0e863a3789655f7d24b5d825d
Saksham Kohli <122266867+sakshamk007@users.noreply.github.com>
2025-02-19 00:02:12 +0530

    contact page

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-
     res.render('pages/index');
 });
 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
 });
 
 app.listen(8080);
